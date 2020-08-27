7ff62c5996d675d9030672ba426f5e51dabf1512
mdNaimulHoque <naimulhoq89@gmail.com>
2020-08-27 22:43:51 +0600

    three dots is done

---
+++ letconst.js
@@ -0,0 +1,13 @@
+const hubby = "Elias Kanchon";
+//console.log(hubby);
+
+const numbers = [12, 45];
+numbers[1] = 88;
+numbers.push(12);
+//numbers = ["Sunny", 'Salman']
+const nayok = {name: 'sakib', phone: 4578};
+//console.log(numbers);
+
+let patienName = 'Rahim Chacha';
+patienName = "Fatema Khala"
+console.log(patienName);
---
+++ template.js
@@ -0,0 +1,14 @@
+const firstName = "Justin";
+const lastName = "TimberLake";
+const fullName = firstName + " " + lastName + " is a good boy";
+const fullName2 = `${firstName} ${20+50+30} is a good boy.`;
+
+const multiLine = `I love you
+I miss you
+no. I don't need you
+Baily road e dorkar nai.`
+
+//console.log(multiLine);
+const name = "Adam Sandler";
+const greeting = `Hello ${name} welcome the club`;
+console.log(greeting);
